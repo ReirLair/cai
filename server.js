5e509b37f36a953fa8bd4ddb9c87a7c19bfdca6e
ReirLair <indiaserverff32@gmail.com>
2025-05-22 11:58:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/welcome', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.post('/register', async (req, res) => {
     const { username, password, socials } = req.body;
     if (!username || !password) return res.status(400).json({ error: 'Username and password are required' });
